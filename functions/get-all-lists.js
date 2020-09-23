52ed6c894309fff3462a5261b5a91c10f64a744c
Jonas Holbech <jh@jonasholbech.dk>
2020-09-23 21:53:19 +0200

    get all lists

---
+++ functions/get-all-lists.js
@@ -0,0 +1,37 @@
+const sendQuery = require("./helpers/send-query");
+
+const GET_ALL_COMMENTS = `
+query {
+    allLists {
+      data {
+        _id
+        _ts
+        title
+        comments {
+          data {
+            _id
+            author
+            comment
+            _ts
+          }
+        }
+      }
+    }
+  }
+`;
+
+exports.handler = async () => {
+  const { data, errors } = await sendQuery(GET_ALL_COMMENTS);
+
+  if (errors) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify(errors),
+    };
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ notes: data.allLists.data }),
+  };
+};
