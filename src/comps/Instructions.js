7fdda07f89b3dd542175d30a08ea399fa4a8e269
Jonas Holbech <jh@jonasholbech.dk>
2020-09-28 13:05:37 +0200

    instructions added

--- src/comps/Instructions.js
+++ src/comps/Instructions.js
@@ -1,10 +1,22 @@
 import React from "react";
+import Button from "muicss/lib/react/button";
 
 export default function Instructions({ onClose }) {
   return (
     <section>
-      <p>Use ```</p>
-      <button onClick={onClose}>Close me!</button>
+      <h4>Sådan skriver du</h4>
+      <h5>Kode</h5>
+      <pre>
+        ```python{"\n"}
+        din kode{"\n"}
+        ```
+      </pre>
+      <h5>Fed tekst</h5>
+      <pre>her kommer **fed** tekst</pre>
+
+      <Button size="small" color="primary" variant="raised" onClick={onClose}>
+        Luk
+      </Button>
     </section>
   );
 }
